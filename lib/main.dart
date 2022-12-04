462223ace7f2e8f290fece2dd22b3b16c69a6668
ketanvishwakarma <ketanvishwakarma@gmail.com>
2022-12-04 11:46:09 +0530

    Add: Hive

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:todo/app/app.dart';
 import 'package:todo/config/dependency_injection/dependency_injection.dart';
 
 void main() async {
+  await Hive.initFlutter();
   await configureDependencies();
   runApp(const App());
 }
