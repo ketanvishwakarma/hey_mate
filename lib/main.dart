b0a35c3775bf34f7a543e95f68884e1222dc6678
ketanvishwakarma <ketanvishwakarma@gmail.com>
2022-12-04 11:40:56 +0530

    Add: dependency injection

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:todo/app/app.dart';
+import 'package:todo/config/dependency_injection/dependency_injection.dart';
 
-void main() {
+void main() async {
+  await configureDependencies();
   runApp(const App());
 }
